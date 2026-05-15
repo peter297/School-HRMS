c5d68ccb46f20fb30828e41c918954e12c474b13
peter297 <peterkimani297@gmail.com>
2026-05-15 12:56:10 +0300

    Initailising the Project

---
+++ app/Models/User.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
+class User extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+
+     protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+     ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * Get the user's initials
+     */
+    public function initials(): string
+    {
+        return Str::of($this->name)
+            ->explode(' ')
+            ->take(2)
+            ->map(fn ($word) => Str::substr($word, 0, 1))
+            ->implode('');
+    }
+
+    public function isSuperAdmin(): bool
+    {
+        return $this->role === 'super_admin';
+    }
+
+    public function isHrAdmin(): bool
+    {
+        return $this->role === 'hr_admin';
+    }
+    
+    public function isStaff(): bool
+    {
+        return $this->role === 'staff';
+    }
+
+    public function canManageHr(){
+        return in_array($this->role, ['super_admin', 'hr_admin']);
+    }
+
+    public function employees(){
+        return $this->belongsToMany(Employee::class);
+
+    }
+}
