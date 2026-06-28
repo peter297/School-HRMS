c5e35f931918973c40f10664ef077a872e2c2058
Kimkimani45 <ppptahkim@gmail.com>
2026-06-28 20:16:16 +0300

    Logo changes

--- resources/views/layouts/auth/split.blade.php
+++ resources/views/layouts/auth/split.blade.php
@@ -6,11 +6,14 @@
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-                <div class="absolute inset-0 bg-neutral-900"></div>
+                <div class="absolute inset-0 bg-neutral-900">
+                    <img src="{{ asset('images/office.png') }}" />
+                </div>
                 <a href="{{ route('login') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
                     <span class="flex h-20 w-20 items-center justify-center rounded-md">
                         {{-- <x-app-logo-icon class="me-2 h-7 fill-current text-white" /> --}}
-                        <img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name', 'Human Resource Management System') }} Logo" class="h-[10px]  w-auto fill-current text-white" />
+                        {{-- <img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name', 'Human Resource Management System') }} Logo" class="h-[10px]  w-auto fill-current text-white" /> --}}
+                        {{-- <x-app-logo-icon class="size-20 fill-current text-black dark:text-white" /> --}}
                     </span>
                     {{-- {{ config('app.name', 'Human Resource Management System') }} --}}
                 </a>
@@ -31,11 +34,11 @@
             <div class="w-full lg:p-8">
                 <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <a href="{{ route('login') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden" wire:navigate>
-                        <span class="flex h-9 w-9 items-center justify-center rounded-md">
-                            <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                        <span class="flex h-20 w-20 items-center justify-center rounded-md">
+                            <x-app-logo-icon class="size-20 fill-current text-black dark:text-white" />
                         </span>
 
-                        <span class="sr-only">{{ config('app.name', 'School HRMS') }}</span>
+                        {{-- <span class="sr-only">{{ config('app.name', 'School HRMS') }}</span> --}}
                     </a>
                     {{ $slot }}
                 </div>
